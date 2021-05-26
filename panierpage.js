f192e5c0d8ea3a418482c30b32928ebd6c980d56
bentaniamin1 <amin.bentani@hetic.net>
2021-05-26 19:46:47 +0200

    ajout du max et du min dans le panier

--- panierpage.js
+++ panierpage.js
@@ -2,43 +2,95 @@ let listeP = localStorage.getItem("listePanier");
 let liste = JSON.parse(listeP);
 
 console.log(liste);
-console.log(liste.length);
+let longeur = liste.length;
+console.log(longeur);
+
 console.log(liste[1].nom);
-console.log(liste[1]);
+let max = 9;
+let min = 0;
 
-for(i = 0 ; liste.length; i++){
-    let html = `<section>
-            <div>
-                <h1>Produit ${[i]} <h1>
-                <img id ="imge"src="${liste[i].image} " alt="">
-                <h1>${liste[i].nom}</h1>
-                <h2>${liste[i].prix} $</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente corrupti molestiae voluptates quis, eum ratione reprehenderit atque vel. Ad, necessitatibus. Eveniet architecto quidem delectus nulla, totam iste sed veritatis eius.</p>
-                <button id = "ajouter${liste[i]}">+</button>
-                <p>0</p>
-                <button id = "diminuer${liste[i]}">-</button>
-            </div>
-        </section>
+function plusButton(index){
     
-    `   ;
-    let main = document.querySelector("main");
-        
-    console.log(liste[i].image);
-    console.log(liste[i].id);
-    main.innerHTML += html;
+    let nombre = document.getElementById('ab'+(index));
+    console.log(nombre);
+    let maxArticles = parseInt(liste[index].max_articles);
+    let nombreValue = parseInt(nombre.innerText);
+    console.log(nombreValue);
+    if(nombreValue < 9){
+        let ajour1 = nombre.innerHTML = nombreValue+ 1;
+    }
+    console.log(ajour1);
 
-    let battoi = document.getElementById('diminuer'+`${liste[i].id}`);
-    console.log(battoi);
+
+
+    //if(liste[i].id > 9){
+        //console.log(liste[i].id);
+   // }
+}
+function moinButton(index){
+    let nombre = document.getElementById('ab'+(index));
+    console.log(nombre);
+    let maxArticles = parseInt(liste[index].max_articles);
+    let nombreValue = parseInt(nombre.innerText);
+    console.log(nombreValue);
+    if(nombreValue > 0){
+
+        let ajour1 = nombre.innerHTML = nombreValue - 1;
+    }
+    else{
+        let ajour2 = nombre.innerHTML =0
+    }
+    console.log(ajour1);
+    
 }
 
 
-battoi1.addEventListener("click",()=>{
-    console.log("ajouter");
-    console.log(liste1);
-    console.log("tas reussi bg");
-    let pr = JSON.parse(localStorage.getItem('listePanier'));
-    pr.push(liste[i]);
-    localStorage.setItem('listePanier', JSON.stringify(pr));
-});
-console.log(i);
+const printPanier = ()=> {
+    
+    for(i = 0 ; liste.length; i++){
+        let html = `<section>
+        <div>
+        <h1>Produit ${[i]} <h1>
+        <img id ="imge"src="${liste[i].image} " alt="">
+        <h1>${liste[i].nom}</h1>
+        <h2>${liste[i].prix} $</h2>
+        <button id = "a` +i+ `" onclick =" plusButton(` +i+ `)">+</button>
+        <p id = "ab` +i+ `">0</p>
+        <button id = "diminuer` +i+ `" onclick ="moinButton(` +i+ `)" >-</button>
+        </div>
+        </section>
+        
+        `   ;
+
+        let main = document.querySelector("main");
+        console.log(liste[i].id);
+        console.log(liste[i].id);
+        main.innerHTML += html;
+        console.log("sdfghjkl");
+        console.log(liste[i].id);
+        let po = `${liste[i].id}`;
+        console.log(po);
+        toString(po);
+        let cc =  "a"+ po;
+        console.log(cc);
+        //while(liste.lenght)
+        let bt = document.getElementById("a");
+        console.log(bt);
+
+    
+    
+    //let battoi = document.getElementById("ajouter");
+    //battoi[i].addEventListener('click',()=>{
+    //});
+    
+}
+
+}
+
+printPanier();
+
+
+//faut recuperer nombres d'articles dans le paniers
+// ensuite faire une condiditon de 0 à 9 pour le noòbre max d'articles dans le panier
+//faire donc un set item du panier
 
